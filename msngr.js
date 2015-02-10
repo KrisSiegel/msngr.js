877d75551b5564976f8cc4a3ebfd3366eb3806a4
Kris Siegel <Kris@BinaryIdiot.com>
2015-02-10 01:28:49 -0500

    Messaging components return!
    Refactoring of core messaging complete.
    Unit tests cover all new methods.

--- msngr.js
+++ msngr.js
@@ -409,15 +409,67 @@ msngr.extend((function () {
 msngr.extend((function () {
     "use strict";
 
+    var delegates = { };
+    var delegateCount = 0;
+
+    var executeSync = function (method, context, params) {
+        method.apply(context, params);
+    };
+
+    var execute = function (method, context, params) {
+        (function (m, c, p) {
+            setTimeout(function () {
+                executeSync(m, c, p);
+            }, 0);
+        }(method, context, params));
+    };
+
     return {
-        emit: function () {
+        emit: function (message, payload) {
+            var uuids = msngr.stores.memory.query(message);
+            if (uuids.length > 0) {
+                for (var i = 0; i < uuids.length; ++i) {
+                    var del = delegates[uuids[i]];
+                    execute(del.callback, del.context, [payload || message.payload]);
+                }
+            }
 
+            return msngr;
         },
-        register: function () {
+        register: function (message, callback) {
+            var uuid = msngr.stores.memory.index(message);
+            delegates[uuid] = {
+                callback: callback,
+                context: (message.context || this),
+                registeredMessage: message
+            };
+            delegateCount++;
 
+            return msngr;
         },
-        unregister: function () {
-            
+        unregister: function (message) {
+            var uuids = msngr.stores.memory.query(message);
+            if (uuids.length > 0) {
+                for (var i = 0; i < uuids.length; ++i) {
+                    var uuid = uuids[i];
+                    delete delegates[uuid];
+                    delegateCount--;
+
+                    msngr.stores.memory.delete(uuid);
+                }
+            }
+
+            return msngr;
+        },
+        unregisterAll: function () {
+            delegates = { };
+            delegateCount = 0;
+            msngr.stores.memory.clear();
+
+            return msngr;
+        },
+        getDelegateCount: function () {
+            return delegateCount;
         }
     };
 }()));
