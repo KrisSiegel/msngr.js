51930200b0d55272b83a8d8bdd3b394a0e721934
Kris Siegel <Kris@BinaryIdiot.com>
2015-03-04 18:07:14 -0500

    Additional unit tests to cover edge cases
    Additional unit tests for the memory store to ensure valid input and
    output in more edge cases.

--- src/store/memory.aspec.js
+++ src/store/memory.aspec.js
@@ -99,7 +99,50 @@ describe("./stores/memory.js", function () {
         expect(msngr.store.count()).to.equal(0);
     });
 
-    it("msngr.store.query(message)", function () {
+    it("msngr.store.query(message) - Correctly gets one result for a query on a topic", function () {
+        var message = {
+            topic: "TestTopic1"
+        };
+
+        var id = msngr.store.index(message);
+        expect(id).to.exist;
+
+        var result = msngr.store.query({ topic: "TestTopic1" });
+
+        expect(result).to.exist;
+        expect(result.length).to.equal(1);
+    });
+
+    it("msngr.store.query(message) - Correctly gets zero results for a query on a topic that doesn't exist", function () {
+        var result = msngr.store.query({ topic: "TestTopic1" });
+
+        expect(result).to.exist;
+        expect(result.length).to.equal(0);
+    });
+
+    it("msngr.store.query(message) - Correctly gets one result for a query on a topic and category", function () {
+        var message = {
+            topic: "TestTopic1",
+            category: "TestCategory1"
+        };
+
+        var id = msngr.store.index(message);
+        expect(id).to.exist;
+
+        var result = msngr.store.query({ topic: "TestTopic1", category: "TestCategory1" });
+
+        expect(result).to.exist;
+        expect(result.length).to.equal(1);
+    });
+
+    it("msngr.store.query(message) - Correctly gets zero results for a query on a topic and category that doesn't exist", function () {
+        var result = msngr.store.query({ topic: "TestTopic1", category: "TestCategory1" });
+
+        expect(result).to.exist;
+        expect(result.length).to.equal(0);
+    });
+
+    it("msngr.store.query(message) - Correctly gets one result for a query on a topic, category and dataType", function () {
         var message = {
             topic: "TestTopic1",
             category: "TestCategory1",
@@ -109,25 +152,92 @@ describe("./stores/memory.js", function () {
         var id = msngr.store.index(message);
         expect(id).to.exist;
 
-        var result1 = msngr.store.query({ topic: "TestTopic1" });
-        var result2 = msngr.store.query({ topic: "TestTopical" });
-        var result3 = msngr.store.query({ topic: "TestTopic1", category: "TestCategory1" });
-        var result4 = msngr.store.query({ topic: "TestTopic1", category: "TestCategoral" });
-        var result5 = msngr.store.query({ topic: "TestTopic1", category: "TestCategory1", dataType: "TestDataType1" });
-        var result6 = msngr.store.query({ topic: "TestTopic1", category: "TestCategory1", dataType: "TestDataTypal" });
+        var result = msngr.store.query({ topic: "TestTopic1", category: "TestCategory1", dataType: "TestDataType1" });
 
-        expect(result1).to.exist;
-        expect(result2).to.exist;
-        expect(result3).to.exist;
-        expect(result4).to.exist;
-        expect(result5).to.exist;
-        expect(result6).to.exist;
+        expect(result).to.exist;
+        expect(result.length).to.equal(1);
+    });
 
-        expect(result1.length).to.equal(1);
-        expect(result2.length).to.equal(0);
-        expect(result3.length).to.equal(1);
-        expect(result4.length).to.equal(0);
-        expect(result5.length).to.equal(1);
-        expect(result6.length).to.equal(0);
+    it("msngr.store.query(message) - Correctly gets zero results for a query on a topic, category and dataType that doesn't exist", function () {
+        var result = msngr.store.query({ topic: "TestTopic1", category: "TestCategory1", dataType: "TestDataType1" });
+
+        expect(result).to.exist;
+        expect(result.length).to.equal(0);
+    });
+
+    it("msngr.store.query(message) - Correctly gets zero results for a query on a topic, category and dataType where category doesn't exist", function () {
+        var message = {
+            topic: "TestTopic1",
+            category: "TestCategory1",
+            dataType: "TestDataType1"
+        };
+
+        var id = msngr.store.index(message);
+        expect(id).to.exist;
+
+        var result = msngr.store.query({ topic: "TestTopic1", category: "TestCategory2", dataType: "TestDataType1" });
+
+        expect(result).to.exist;
+        expect(result.length).to.equal(0);
+    });
+
+    it("msngr.store.query(message) - Correctly gets zero results for a query on a topic, category and dataType where dataType doesn't exist", function () {
+        var message = {
+            topic: "TestTopic1",
+            category: "TestCategory1",
+            dataType: "TestDataType1"
+        };
+
+        var id = msngr.store.index(message);
+        expect(id).to.exist;
+
+        var result = msngr.store.query({ topic: "TestTopic1", category: "TestCategory1", dataType: "TestDataType2" });
+
+        expect(result).to.exist;
+        expect(result.length).to.equal(0);
+    });
+
+    it("msngr.store.query(message) - Correctly gets zero results for a query on a topic, category and dataType where topic doesn't exist", function () {
+        var message = {
+            topic: "TestTopic1",
+            category: "TestCategory1",
+            dataType: "TestDataType1"
+        };
+
+        var id = msngr.store.index(message);
+        expect(id).to.exist;
+
+        var result = msngr.store.query({ topic: "TestTopic2", category: "TestCategory1", dataType: "TestDataType1" });
+
+        expect(result).to.exist;
+        expect(result.length).to.equal(0);
+    });
+
+    it("msngr.store.clear() - Clears all indexed messages", function () {
+        var ids = [];
+        ids.push(msngr.store.index({ topic: "chicken" }));
+        ids.push(msngr.store.index({ topic: "table", category: "50" }));
+        ids.push(msngr.store.index({ topic: "stairs", category: "10", dataType: "food" }));
+        ids.push(msngr.store.index({ topic: "whatevea", dataType: "punk" }));
+
+        expect(msngr.store.count()).to.equal(4);
+        expect(msngr.store.clear()).to.equal(true);
+        expect(msngr.store.query({ topic: "chicken" }).length).to.equal(0);
+        expect(msngr.store.query({ topic: "stairs", category: "10", dataType: "food" }).length).to.equal(0);
+    });
+
+    it("msngr.store.count() - Returns a correct count", function () {
+        expect(msngr.store.count()).to.equal(0);
+        msngr.store.index({ topic: "chicken" });
+        expect(msngr.store.count()).to.equal(1);
+        msngr.store.index({ topic: "table", category: "50" });
+        expect(msngr.store.count()).to.equal(2);
+        msngr.store.index({ topic: "stairs", category: "10", dataType: "food" });
+        expect(msngr.store.count()).to.equal(3);
+        expect(msngr.store.clear()).to.equal(true);
+        expect(msngr.store.count()).to.equal(0);
+        msngr.store.index({ topic: "whatevea", dataType: "punk" });
+        expect(msngr.store.count()).to.equal(1);
+ 
     });
 });
