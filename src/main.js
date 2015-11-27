e8a23149556ccc54f06b74907b25d496eb91616f
Kris Siegel <KrisSiegel@Gmail.com>
2015-11-26 23:17:06 -0500

    Changing version to 3.2.0

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ var msngr = msngr || (function() {
         return internal.objects.message(topic, category, subcategory);
     };
 
-    external.version = "4.0.0";
+    external.version = "3.2.0";
 
     var getType = function(input) {
         return Object.prototype.toString.call(input);
