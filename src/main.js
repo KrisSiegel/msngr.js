6a29a00c320f65459ead593357c6751b33ad8f95
Kris Siegel <KrisSiegel@Gmail.com>
2016-11-27 00:55:49 -0800

    Version++ gitignore yarn.lock

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ var msngr = msngr || (function () {
     };
 
     // Built version of msngr.js for programatic access; this is auto generated
-    external.version = "5.0.1";
+    external.version = "5.1.0";
 
     // Takes a function, executes it passing in the external and internal interfaces
     external.extend = function (fn) {
